2c7ca09b911363e493df83bf6d0dfc1435304766
henryz2000 <hzheng3@stuy.edu>
2018-02-28 11:08:00 -0500

    finish matrix using ident

---
+++ main.py
@@ -0,0 +1,17 @@
+from display import *
+from draw import *
+from parser import *
+from matrix import *
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+edges = []
+transform = new_matrix()
+
+#dw test file
+parse_file( 'script', edges, transform, screen, color )
+
+'''
+#henry test file
+parse_file( 'insert file name here', edges, transform, screen, color )
+'''
